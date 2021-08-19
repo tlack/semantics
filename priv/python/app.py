18b6c264ec2420f3b32d71d6339737ce8bc4a454
tlack <lackner@gmail.com>
2021-08-19 20:15:27 +0000

    support calculating embeddings for lists of texts

--- priv/python/app.py
+++ priv/python/app.py
@@ -19,29 +19,26 @@ def load(model_name):
     global CUR_MODEL_NAME, CUR_MODEL_OBJ
     model_name = unpack(model_name)
     if model_name != CUR_MODEL_NAME:
-       print('SEMANTICS: loading model',model_name)
+       print('🚨 SEMANTICS: loading model',model_name)
        CUR_MODEL_OBJ = SentenceTransformer(model_name)
        CUR_MODEL_NAME = model_name
     return CUR_MODEL_OBJ
 
 def predict_many(model, texts):
     text2 = [unpack(x) for x in texts]
-    test_emb = model.encode(text2, show_progress_bar=False)
-    return test_emb.tolist()
+    emb = model.encode(text2, show_progress_bar=False)
+    return emb.tolist()
 
 def predict_one(model, text):
     text = unpack(text)
-    test_emb = model.encode([text], show_progress_bar=False)
-    # print('emb', test_emb)
-    return test_emb[0].tolist()
+    emb = model.encode([text], show_progress_bar=False)
+    return emb[0].tolist()
 
 def predict(model_name, text):
     model = load(model_name)
-    if type(text) == type([]):
+    if type(text) == erlport.erlterms.List:
         return predict_many(model, text)
     else:
         return predict_one(model, text)
-
-
 
 
