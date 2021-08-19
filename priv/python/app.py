fd17efbae615cfb97f9a725bc128428e524c5c33
tlack <lackner@gmail.com>
2021-08-19 19:44:27 +0000

    ability to get embeddings for multiple texts

--- priv/python/app.py
+++ priv/python/app.py
@@ -29,11 +29,24 @@ def load(model_name):
        CUR_MODEL_NAME = model_name
     return CUR_MODEL_OBJ
 
-def predict(model_name, text):
+def predict_many(model, texts):
+    text2 = [unpack(x) for x in texts]
+    test_emb = model.encode(text2, show_progress_bar=False)
+    return test_emb.tolist()
+
+def predict_one(model, text):
     text = unpack(text)
-    model = load(model_name)
     test_emb = model.encode([text], show_progress_bar=False)
     # print('emb', test_emb)
-    return list([float(x) for x in test_emb[0]])
+    return test_emb[0].tolist()
+
+def predict(model_name, text):
+    model = load(model_name)
+    if type(text) == type([]):
+        return predict_many(model, text)
+    else:
+        return predict_one(model, text)
+
+
 
 
